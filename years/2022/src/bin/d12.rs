369960493d835ac03ce55b81e05968a1e0c5359a
filipandersson245 <jabos123@gmail.com>
2022-12-12 09:36:30 +0100

    Optimized p2 by 200x

--- years/2022/src/bin/d12.rs
+++ years/2022/src/bin/d12.rs
@@ -1,41 +1,7 @@
-use std::collections::VecDeque;
+use std::collections::HashSet;
 
 use lib::prelude::*;
-use pathfinding::prelude::astar;
-use regex::Regex;
-
-fn nr_of_steps(grid: &Vec<Vec<u8>>, start_pos: (usize, usize), end_pos: (usize, usize)) -> usize {
-    let grid_height = grid.len();
-    let grid_width = grid[0].len();
-
-    astar(
-        &start_pos,
-        |&(x_c, y_c)| {
-            let current = grid[x_c][y_c];
-            vec![
-                (x_c.wrapping_sub(1), y_c, current),
-                (x_c + 1, y_c, current),
-                (x_c, y_c + 1, current),
-                (x_c, y_c.wrapping_sub(1), current),
-            ]
-            .into_iter()
-            .filter_map(|(x, y, current)| {
-                if x >= grid_height || y >= grid_width {
-                    return None;
-                }
-                let other = grid[x][y];
-                if current + 1 >= other {
-                    return Some(((x, y), 1));
-                }
-                return None;
-            })
-        },
-        |&(x, y)| (end_pos.0.abs_diff(x) + end_pos.1.abs_diff(y)),
-        |&p| p == end_pos,
-    )
-    .unwrap_or_else(|| (vec![], usize::MAX))
-    .1
-}
+use pathfinding::prelude::{astar, bfs};
 
 pub fn part_one(input: &str) -> Result<usize> {
     let mut grid = input
@@ -63,7 +29,33 @@ pub fn part_one(input: &str) -> Result<usize> {
             }
         }
     }
-    Ok(nr_of_steps(&grid, start_pos, end_pos))
+    Ok(astar(
+        &start_pos,
+        |&(x_c, y_c)| {
+            let current = grid[x_c][y_c];
+            vec![
+                (x_c.wrapping_sub(1), y_c, current),
+                (x_c + 1, y_c, current),
+                (x_c, y_c + 1, current),
+                (x_c, y_c.wrapping_sub(1), current),
+            ]
+            .into_iter()
+            .filter_map(|(x, y, current)| {
+                if x >= grid_height || y >= grid_width {
+                    return None;
+                }
+                let other = grid[x][y];
+                if current + 1 >= other {
+                    return Some(((x, y), 1));
+                }
+                return None;
+            })
+        },
+        |&(x, y)| (end_pos.0.abs_diff(x) + end_pos.1.abs_diff(y)),
+        |&p| p == end_pos,
+    )
+    .unwrap_or_else(|| (vec![], usize::MAX))
+    .1)
 }
 
 pub fn part_two(input: &str) -> Result<usize> {
@@ -93,19 +85,37 @@ pub fn part_two(input: &str) -> Result<usize> {
         }
     }
 
-    let res = multiple_start_pos
-        .iter()
-        .map(|&start_pos| nr_of_steps(&grid, start_pos, end_pos))
-        .min()
-        .unwrap();
-    Ok(res)
-}
+    let multiple_start_pos: HashSet<_> = HashSet::from_iter(multiple_start_pos.into_iter());
 
-pub fn parse(input: &str) -> Vec<Vec<u8>> {
-    input
-        .split("\n")
-        .map(|a| a.as_bytes().to_vec())
-        .collect_vec()
+    let res = bfs(
+        &end_pos,
+        |&(x_c, y_c)| {
+            let current = grid[x_c][y_c];
+            vec![
+                (x_c.wrapping_sub(1), y_c, current),
+                (x_c + 1, y_c, current),
+                (x_c, y_c + 1, current),
+                (x_c, y_c.wrapping_sub(1), current),
+            ]
+            .into_iter()
+            .filter_map(|(x, y, current)| {
+                if x >= grid_height || y >= grid_width {
+                    return None;
+                }
+                let other = grid[x][y];
+                if current <= other + 1 {
+                    return Some((x, y));
+                }
+                return None;
+            })
+        },
+        |&p| multiple_start_pos.contains(&p),
+    )
+    .unwrap_or_else(|| (vec![]))
+    .len()
+        - 1;
+
+    Ok(res)
 }
 
 fn main() -> Result<()> {
@@ -117,7 +127,7 @@ fn main() -> Result<()> {
 }
 
 // part_one: 408
-//  took 234.04µs
+//  took 249.759µs
 
 // part_two: 399
-//  took 42.561435ms
+//  took 207.564µs
