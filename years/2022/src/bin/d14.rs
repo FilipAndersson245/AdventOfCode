51e1cad67fe24827c05b7eb8cba2ca1dce1a8087
filipandersson245 <jabos123@gmail.com>
2022-12-14 09:55:35 +0100

    Made it work alot faster :)

--- years/2022/src/bin/d14.rs
+++ years/2022/src/bin/d14.rs
@@ -1,6 +1,3 @@
-use fasthash::sea::Hash64;
-use std::collections::HashMap;
-
 use lib::prelude::*;
 
 fn parse_point(s: &str) -> (usize, usize) {
@@ -14,10 +11,9 @@ fn min_to_max(a: usize, b: usize) -> std::ops::RangeInclusive<usize> {
     a.min(b)..=a.max(b)
 }
 
-pub fn part_one(input: &str) -> Result<usize> {
-    // let mut grid = vec![vec![' '; 1000]; 1000];
+fn init_grid(input: &str) -> (Vec<Vec<bool>>, usize) {
+    let mut grid_map = vec![vec![true; 1000]; 1000];
     let mut largestst_y = 0;
-    let mut grid_map = HashMap::with_capacity_and_hasher(8192, Hash64);
     input.lines().for_each(|line| {
         let mut iter = line.split(" -> ").map(parse_point);
         let mut from = iter.next().unwrap();
@@ -28,12 +24,26 @@ pub fn part_one(input: &str) -> Result<usize> {
                     if y > largestst_y {
                         largestst_y = y;
                     }
-                    grid_map.insert((x, y), '#');
+                    grid_map[x][y] = false
                 }
             }
             from = to;
         }
     });
+    (grid_map, largestst_y)
+}
+
+fn get_possible_sand_locations(
+    sand_pos: (usize, usize),
+) -> ((usize, usize), (usize, usize), (usize, usize)) {
+    let sand_down = (sand_pos.0, sand_pos.1 + 1);
+    let sand_down_left = (sand_pos.0 - 1, sand_pos.1 + 1);
+    let sand_down_right = (sand_pos.0 + 1, sand_pos.1 + 1);
+    (sand_down, sand_down_left, sand_down_right)
+}
+
+pub fn part_one(input: &str) -> Result<usize> {
+    let (mut grid_map, largestst_y) = init_grid(input);
 
     let sand_starting_pos = (500, 0);
     let mut sand = 0;
@@ -44,18 +54,17 @@ pub fn part_one(input: &str) -> Result<usize> {
                 break 'outer;
             }
 
-            let sand_down = (sand_pos.0, sand_pos.1 + 1);
-            let sand_down_left = (sand_pos.0 - 1, sand_pos.1 + 1);
-            let sand_down_right = (sand_pos.0 + 1, sand_pos.1 + 1);
+            let (sand_down, sand_down_left, sand_down_right) =
+                get_possible_sand_locations(sand_pos);
 
-            if !grid_map.contains_key(&sand_down) {
+            if grid_map[sand_down.0][sand_down.1] {
                 sand_pos = sand_down;
-            } else if !grid_map.contains_key(&sand_down_left) {
+            } else if grid_map[sand_down_left.0][sand_down_left.1] {
                 sand_pos = sand_down_left;
-            } else if !grid_map.contains_key(&sand_down_right) {
+            } else if grid_map[sand_down_right.0][sand_down_right.1] {
                 sand_pos = sand_down_right;
             } else {
-                grid_map.insert(sand_pos, '+');
+                grid_map[sand_pos.0][sand_pos.1] = false;
                 sand += 1;
                 break;
             }
@@ -65,48 +74,30 @@ pub fn part_one(input: &str) -> Result<usize> {
 }
 
 pub fn part_two(input: &str) -> Result<usize> {
-    // let mut grid = vec![vec![' '; 1000]; 1000];
-    let mut largestst_y = 0;
-    let mut grid_map = HashMap::with_capacity_and_hasher(8192, Hash64);
-    input.lines().for_each(|line| {
-        let mut iter = line.split(" -> ").map(parse_point);
-        let mut from = iter.next().unwrap();
-
-        while let Some(to) = iter.next() {
-            for x in min_to_max(from.0, to.0) {
-                for y in min_to_max(from.1, to.1) {
-                    if y > largestst_y {
-                        largestst_y = y;
-                    }
-                    grid_map.insert((x, y), '#');
-                }
-            }
-            from = to;
-        }
-    });
+    let (mut grid_map, largestst_y) = init_grid(input);
 
     let lava = largestst_y + 2;
-    for x in 0..=1000 {
-        grid_map.insert((x, lava), '#');
+    for x in 0..1000 {
+        grid_map[x][lava] = false
     }
 
     let sand_starting_pos = (500, 0);
     let mut sand = 0;
+
     'outer: loop {
         let mut sand_pos = sand_starting_pos;
         loop {
-            let sand_down = (sand_pos.0, sand_pos.1 + 1);
-            let sand_down_left = (sand_pos.0 - 1, sand_pos.1 + 1);
-            let sand_down_right = (sand_pos.0 + 1, sand_pos.1 + 1);
+            let (sand_down, sand_down_left, sand_down_right) =
+                get_possible_sand_locations(sand_pos);
 
-            if !grid_map.contains_key(&sand_down) {
+            if grid_map[sand_down.0][sand_down.1] {
                 sand_pos = sand_down;
-            } else if !grid_map.contains_key(&sand_down_left) {
+            } else if grid_map[sand_down_left.0][sand_down_left.1] {
                 sand_pos = sand_down_left;
-            } else if !grid_map.contains_key(&sand_down_right) {
+            } else if grid_map[sand_down_right.0][sand_down_right.1] {
                 sand_pos = sand_down_right;
             } else {
-                grid_map.insert(sand_pos, '+');
+                grid_map[sand_pos.0][sand_pos.1] = false;
                 sand += 1;
                 if sand_pos == sand_starting_pos {
                     break 'outer;
@@ -127,7 +118,7 @@ fn main() -> Result<()> {
 }
 
 // part_one: 614
-//  took 367.664µs
+//  took 178.109µs
 
 // part_two: 26170
-//  took 18.195992ms
+//  took 2.60146ms
